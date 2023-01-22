66effbfcf5f7c356cca37b444ac135fa0d8fa003
ashkanazimi1991 <ashkan1991azimi@gmail.com>
2023-01-22 13:23:51 +0330

    wew

--- next.config.js
+++ next.config.js
@@ -1,14 +1,9 @@
-/** @type {import('next').NextConfig} */
-
-const withPWA = require('next-pwa')({
-  dest: 'public'
-  // disable: process.env.NODE_ENV === 'development',
-  // register: true,
-  // scope: '/app',
-  // sw: 'service-worker.js',
-  //...
-})
-
-module.exports = withPWA({
-  // next.js config
-})
+module.exports = (phase, { defaultConfig }) => {
+  /**
+   * @type {import('next').NextConfig}
+   */
+  const nextConfig = {
+    /* config options here */
+  }
+  return nextConfig
+}
